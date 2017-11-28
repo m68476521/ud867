82830733b416edc4aacd9e6b9a12979698e07f7e
Mike <m68476521.1@gmail.com>
2017-11-27 21:54:45 -0800

    feat: step 1
    created a Java library that provide a joke

---
+++ FinalProject/JokeProvider/src/main/java/com/m68476521/mike/jokeprovider/Joker.java
@@ -0,0 +1,9 @@
+package com.m68476521.mike.jokeprovider;
+
+public class Joker {
+    public String getJoker() {
+        return "Q. Why do the French like to eat snails so much?" +
+                "-" +
+                "A. They can’t stand fast food.";
+    }
+}
--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.m68476521.mike.jokeprovider.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,8 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker joker = new Joker();
+        Toast.makeText(this, joker.getJoker(), Toast.LENGTH_SHORT).show();
     }
-
-
 }
